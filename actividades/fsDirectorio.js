2781925ba723440072de4fb6a01e743d2a2f4198
Darkin03 <esnaider.ortega@unisinu.edu.co>
2024-09-25 09:24:21 -0500

    Terminacion del fsDirectorio.js

--- actividades/fsDirectorio.js
+++ actividades/fsDirectorio.js
@@ -1,33 +1,40 @@
 const fs = require ('node:fs/promises')
-
+const path = require ('node:path')
 const folder = process.argv[2] ?? '.';
 
 
 
-async function ls(directorio){
-
+async function ls(folder){
+    let files; 
     try {
-        const files = await fs.readdir(folder)
+         files = await fs.readdir(folder)
     } catch (error) {
-        console.error('Error al leet el directorio: ',err)
+        console.error(`Error al leer el directorio: ${folder}`);
+        process.exit(1);
     }
 
-    const files = await fs.readdir(folder)
-    .then(
-        files =>{
-            files.forEach(file=>{
-                const filePath=  path.join(folder,file);
-                fs.stat(filePath)
-            })
-        }
-    )
-    
-    .catch(
-        err=>{
-            if(err){
-                console.error('Error al leet el directorio: ',err)
-            }
-        }
-    )
+
+    const filesPromises = files.map(async file => {
+    const filePath = path.join(folder, file);
+    let stats 
+       try {
+        stats = await fs.stat(filePath);
+       } catch (error) {
+         console.log(`Error al obtener el archivo: ${filePath}`);
+       }
+
+       const isDirectory = stats.isDirectory() ? '/' : '';
+       const fileType = isDirectory ? 'Directorio' : 'Archivo';
+       const filesize = stats.size;
+       const fileModified = stats.mtime.toLocaleString();
+
+       return `${file}  ${fileType.padEnd(20)}  ${filesize.toString().padStart(10)} ${fileModified}`;
+
+    })
+
+    const filesInfo = await Promise.all(filesPromises);
+    filesInfo.forEach(fileInfo => console.log(fileInfo));
+
+   
 }
 ls (folder)
